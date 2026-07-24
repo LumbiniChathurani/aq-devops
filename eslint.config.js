a074cf45556a84a880cd812601bc3c7efcdd0050
Lumbini Chathurani <lumbinichathurani@gmail.com>
2026-07-24 15:20:58 +0530

    fix: add jest and timer globals to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,25 @@ export default [
       globals: {
         process: 'readonly',
         console: 'readonly',
+        setTimeout: 'readonly',
       },
     },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
+    },
+  },
+  {
+    files: ['src/__tests__/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
     },
   },
 ];
